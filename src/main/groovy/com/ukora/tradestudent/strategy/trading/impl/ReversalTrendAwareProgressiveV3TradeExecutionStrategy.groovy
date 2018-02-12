dae1ee351fb5f1aef065090d5be13f45e09a7d55
Bas Kuis <b@ukora.com>
2018-02-11 21:18:23 -0600

    added reversal trend aware algo option

---
+++ src/main/groovy/com/ukora/tradestudent/strategy/trading/impl/ReversalTrendAwareProgressiveV3TradeExecutionStrategy.groovy
@@ -0,0 +1,164 @@
+package com.ukora.tradestudent.strategy.trading.impl
+
+import com.ukora.tradestudent.entities.CorrelationAssociation
+import com.ukora.tradestudent.services.simulator.Simulation
+import com.ukora.tradestudent.strategy.trading.TradeExecution
+import com.ukora.tradestudent.strategy.trading.TradeExecutionStrategy
+import com.ukora.tradestudent.tags.TagSubset
+import com.ukora.tradestudent.tags.buysell.BuySellTagGroup
+import com.ukora.tradestudent.tags.buysell.BuyTag
+import com.ukora.tradestudent.tags.buysell.SellTag
+import com.ukora.tradestudent.tags.reversal.DownReversalTag
+import com.ukora.tradestudent.tags.reversal.UpReversalTag
+import com.ukora.tradestudent.tags.trend.DownTag
+import com.ukora.tradestudent.tags.trend.UpTag
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+/**
+ * This trade strategy combines reversal, trend and short term buy sell associations in it's consideration
+ * It's trading strategy is progressive - it will trade according to it's distance from threshold
+ *
+ * This version uses a different reversal weight - and a higher trend division factor and
+ * a more aggressive progressive factor
+ */
+@Component
+class ReversalTrendAwareProgressiveV3TradeExecutionStrategy  implements TradeExecutionStrategy, TagSubset {
+
+    /**
+     * Implementation specific settings
+     */
+    private final Double TREND_REVERSAL_DIVISION_FACTOR = 4
+    private final Double TREND_WEIGHT = 1
+    private final Double REVERSAL_WEIGHT = 3
+
+    /**
+     * Progressive trading settings
+     */
+    final static Double MAX_MULTIPLIER = 8
+    final static Double MIN_MULTIPLIER = 0.1
+
+    @Autowired
+    BuyTag buyTag
+
+    @Autowired
+    SellTag sellTag
+
+    @Autowired
+    BuySellTagGroup buySellTagGroup
+
+    @Autowired
+    UpTag upTag
+
+    @Autowired
+    DownTag downTag
+
+    @Autowired
+    UpReversalTag upReversalTag
+
+    @Autowired
+    DownReversalTag downReversalTag
+
+    @Override
+    String getAlias() {
+        return "duncan"
+    }
+
+    @Override
+    boolean applies(String toTag) {
+        return buySellTagGroup.applies(toTag)
+    }
+
+    private boolean enabled = true
+
+    @Override
+    boolean isEnabled() {
+        return enabled
+    }
+
+    @Override
+    void setEnabled(boolean enabled) {
+        this.enabled = enabled
+    }
+
+    /**
+     * Execute trades while taking reversal probabilities into account
+     *
+     * @param correlationAssociation
+     * @param tag
+     * @param probability
+     * @param simulation
+     * @param combinerStrategy
+     * @param balanceProportion
+     * @return
+     */
+    @Override
+    TradeExecution getTrade(
+            CorrelationAssociation correlationAssociation,
+            String tag,
+            Double probability,
+            Simulation simulation,
+            String combinerStrategy,
+            Double balanceProportion
+    ) {
+        TradeExecution tradeExecution = null
+
+        /**
+         * Get reversal probabilities
+         */
+        Double upReversalProbability = correlationAssociation.tagProbabilities?.get(combinerStrategy)?.get(upReversalTag.tagName)
+        Double downReversalProbability = correlationAssociation.tagProbabilities?.get(combinerStrategy)?.get(downReversalTag.tagName)
+
+        /**
+         * Get trend probabilities
+         */
+        Double upProbability = correlationAssociation.tagProbabilities?.get(combinerStrategy)?.get(upTag.tagName)
+        Double downProbability = correlationAssociation.tagProbabilities?.get(combinerStrategy)?.get(downTag.tagName)
+
+        if (upReversalProbability && downReversalProbability && upProbability && downProbability) {
+
+            /** Calculate aggregate modifier */
+            Double aggregateUpReversalTrendProbability = ((TREND_WEIGHT * upProbability) + (REVERSAL_WEIGHT * upReversalProbability)) / (TREND_WEIGHT + REVERSAL_WEIGHT)
+            Double aggregateDownReversalTrendProbability = ((TREND_WEIGHT * downProbability) + (REVERSAL_WEIGHT * downReversalProbability)) / (TREND_WEIGHT + REVERSAL_WEIGHT)
+
+            /** Modified thresholds */
+            Double modifiedBuyThreshold
+            Double modifiedSellThreshold
+
+            /** Determine if trade execution is relevant */
+            if (aggregateUpReversalTrendProbability > aggregateDownReversalTrendProbability) {
+                Double trendDelta = aggregateUpReversalTrendProbability - aggregateDownReversalTrendProbability
+                modifiedBuyThreshold = simulation.buyThreshold - (trendDelta * (1 - simulation.buyThreshold) / TREND_REVERSAL_DIVISION_FACTOR)
+                modifiedSellThreshold = simulation.sellThreshold + (trendDelta * (1 - simulation.sellThreshold) / TREND_REVERSAL_DIVISION_FACTOR)
+            } else {
+                Double trendDelta = aggregateDownReversalTrendProbability - aggregateUpReversalTrendProbability
+                modifiedBuyThreshold = simulation.buyThreshold + (trendDelta * (1 - simulation.buyThreshold) / TREND_REVERSAL_DIVISION_FACTOR)
+                modifiedSellThreshold = simulation.sellThreshold - (trendDelta * (1 - simulation.sellThreshold) / TREND_REVERSAL_DIVISION_FACTOR)
+            }
+            if (tag == buyTag.getTagName() && probability > modifiedBuyThreshold) {
+                Double buyThresholdDistance = 1 - modifiedBuyThreshold
+                Double actualBuyThresholdDistance = probability - modifiedBuyThreshold
+                Double buyMultiplier = MIN_MULTIPLIER + ((actualBuyThresholdDistance / buyThresholdDistance) * (MAX_MULTIPLIER - MIN_MULTIPLIER))
+                tradeExecution = new TradeExecution(
+                        tradeType: TradeExecution.TradeType.BUY,
+                        amount: buyMultiplier * simulation.tradeIncrement,
+                        price: correlationAssociation.price,
+                        date: correlationAssociation.date
+                )
+            } else if (tag == sellTag.getTagName() && probability > modifiedSellThreshold) {
+                Double sellThresholdDistance = 1 - modifiedSellThreshold
+                Double actualSellThresholdDistance = probability - modifiedSellThreshold
+                Double sellMultiplier = MIN_MULTIPLIER + ((actualSellThresholdDistance / sellThresholdDistance) * (MAX_MULTIPLIER - MIN_MULTIPLIER))
+                tradeExecution = new TradeExecution(
+                        tradeType: TradeExecution.TradeType.SELL,
+                        amount: sellMultiplier * simulation.tradeIncrement,
+                        price: correlationAssociation.price,
+                        date: correlationAssociation.date
+                )
+            }
+
+        }
+        return tradeExecution
+    }
+
+}
---
+++ src/test/groovy/com/ukora/tradestudent/strategy/trading/impl/ReversalTrendAwareProgressiveV3TradeExecutionStrategySpec.groovy
@@ -0,0 +1,86 @@
+package com.ukora.tradestudent.strategy.trading.impl
+
+import com.ukora.tradestudent.entities.CorrelationAssociation
+import com.ukora.tradestudent.services.simulator.Simulation
+import com.ukora.tradestudent.strategy.trading.TradeExecution
+import com.ukora.tradestudent.strategy.trading.TradeExecutionStrategy
+import com.ukora.tradestudent.tags.buysell.BuyTag
+import com.ukora.tradestudent.tags.buysell.SellTag
+import com.ukora.tradestudent.tags.reversal.DownReversalTag
+import com.ukora.tradestudent.tags.reversal.UpReversalTag
+import com.ukora.tradestudent.tags.trend.DownTag
+import com.ukora.tradestudent.tags.trend.UpTag
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class ReversalTrendAwareProgressiveV3TradeExecutionStrategySpec extends Specification {
+
+    TradeExecutionStrategy tradeExecutionStrategy
+
+    def setup() {
+        tradeExecutionStrategy = new ReversalTrendAwareProgressiveV3TradeExecutionStrategy(
+                upTag: new UpTag(),
+                downTag: new DownTag(),
+                buyTag: new BuyTag(),
+                sellTag: new SellTag(),
+                upReversalTag: new UpReversalTag(),
+                downReversalTag: new DownReversalTag()
+        )
+    }
+
+    @Unroll
+    def "test getTrade #tradeType #scenario with p: #probability produces amount: #finalAmount"() {
+
+        setup:
+        CorrelationAssociation correlationAssociation = new CorrelationAssociation(
+                tagProbabilities: [
+                        averageProbabilityCombinerStrategy: [
+                                buy         : buyProbability,
+                                sell        : sellProbablity,
+                                up          : upProbability,
+                                down        : downProbability,
+                                upreversal  : upReversalProbability,
+                                downreversal: downReversalProbability
+                        ]
+                ]
+        )
+        Simulation simulation = new Simulation(
+                tradeIncrement: amount,
+                buyThreshold: buyThreshold,
+                sellThreshold: sellThreshold
+        )
+
+
+        when:
+        TradeExecution tradeExecution = tradeExecutionStrategy.getTrade(
+                correlationAssociation,
+                tag,
+                probability,
+                simulation,
+                combinerStrategy,
+                0.5
+        )
+
+        then:
+        if (!finalAmount) assert !tradeExecution
+        if (finalAmount) {
+            assert tradeExecution.tradeType == tradeType
+            assert tradeExecution.amount == finalAmount
+        }
+
+        where:
+        scenario                                    | tag    | tradeType                     | amount | buyThreshold | sellThreshold | combinerStrategy                     | buyProbability | sellProbablity | upProbability | downProbability | upReversalProbability | downReversalProbability | probability | finalAmount
+        "Up likely lowers buy threshold"            | "buy"  | TradeExecution.TradeType.BUY  | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.6d           | 0.4d           | 0.6d          | 0.4d            | 0.6d                  | 0.4d                    | 0.56d       | 0.4137142857142861
+        "Up likely lowers buy threshold"            | "buy"  | TradeExecution.TradeType.BUY  | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.6d           | 0.4d           | 0.6d          | 0.4d            | 0.6d                  | 0.4d                    | 0.34d       | null
+        "Up/Down balances keeps buy threshold"      | "buy"  | TradeExecution.TradeType.BUY  | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.6d           | 0.4d           | 0.5d          | 0.5d            | 0.5d                  | 0.5d                    | 0.51d       | 0.07740000000000004
+        "Up/Down balances keeps buy threshold"      | "buy"  | TradeExecution.TradeType.BUY  | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.6d           | 0.4d           | 0.5d          | 0.5d            | 0.5d                  | 0.5d                    | 0.49d       | null
+        "Down likely lowers buy threshold"          | "sell" | TradeExecution.TradeType.SELL | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.4d           | 0.6d           | 0.4d          | 0.6d            | 0.4d                  | 0.6d                    | 0.56d       | 0.4137142857142861
+        "Down likely lowers buy threshold"          | "sell" | TradeExecution.TradeType.SELL | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.4d           | 0.6d           | 0.4d          | 0.6d            | 0.4d                  | 0.6d                    | 0.44d       | null
+        "Down likely, sell likely increases amount" | "sell" | TradeExecution.TradeType.SELL | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.2d           | 0.8d           | 0.4d          | 0.6d            | 0.4d                  | 0.6d                    | 0.80d       | 1.4971428571428573
+        "Down likely, sell likely increases amount" | "sell" | TradeExecution.TradeType.SELL | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.1d           | 0.9d           | 0.4d          | 0.6d            | 0.4d                  | 0.6d                    | 0.90d       | 1.9485714285714284
+        "Down likely, sell likely increases amount" | "sell" | TradeExecution.TradeType.SELL | 0.3    | 0.5          | 0.5           | "averageProbabilityCombinerStrategy" | 0.0d           | 1.0d           | 0.4d          | 0.6d            | 0.4d                  | 0.6d                    | 1.00d       | 2.4
+
+    }
+
+}
