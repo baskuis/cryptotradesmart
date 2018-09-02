8cbc3e8d66b4df31f10a51a17ec50f33be0ab9eb
Bas Kuis <bas.kuis@bsci.com>
2018-09-02 12:33:37 -0500

    added databind dep due for java 10

--- src/main/groovy/com/ukora/tradestudent/TradestudentApplication.groovy
+++ src/main/groovy/com/ukora/tradestudent/TradestudentApplication.groovy
@@ -1,32 +1,29 @@
 package com.ukora.tradestudent
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
-@SpringBootApplication(exclude=[
-		SecurityAutoConfiguration.class])
-@EnableAutoConfiguration(exclude=[
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class])
+@SpringBootApplication(exclude = [
+        SecurityAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+])
 class TradestudentApplication {
 
-	public static boolean DEBUG_LOGGING_ENABLED = false
+    public static boolean DEBUG_LOGGING_ENABLED = false
 
-	public static boolean CONSIDER_TWITTER = false
+    public static boolean CONSIDER_TWITTER = false
 
-	public static boolean CONSIDER_NEWS = false
+    public static boolean CONSIDER_NEWS = false
 
-	static void main(String[] args) {
-		println new Date()
-		SpringApplication.run TradestudentApplication, args
-	}
+    static void main(String[] args) {
+        println new Date()
+        SpringApplication.run TradestudentApplication, args
+    }
 
 }
