e6320f877a3272399c41b23480872258c68fe9ab
bukora <b@ukora.com>
2018-12-30 20:52:12 -0600

    khub push

--- refresh.sh
+++ refresh.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m 'khub push' && git pull && git push && ./stopAll.sh && ./gradlew build -xtest && sh collect-app/respawn.sh && sh learn-app/respawn.sh
+git add . ; git commit -m 'khub push' ; git pull ; git push ; ./stopAll.sh && ./gradlew build -xtest && sh collect-app/respawn.sh && sh learn-app/respawn.sh
