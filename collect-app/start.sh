f37dcf09bd6f55e6bab7d1a3548bbe2df8852187
bukora <b@ukora.com>
2019-03-24 13:46:33 -0500

    collector

--- collect-app/start.sh
+++ collect-app/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/nohup /usr/bin/java -jar /home/bas/Projects/tradestudent-learn/collect-app/build/libs/collect-app.jar -Xms4000m -Xmx7000m -XX:-UseGCOverheadLimit >/home/bas/Projects/tradestudent-learn/collect-app/application.out 2>&1 &
+/usr/bin/nohup /usr/bin/java -jar /home/bas/Projects/tradestudent-learn/collect-app/build/libs/collect-app-0.0.1-SNAPSHOT.jar -Xms4000m -Xmx7000m -XX:-UseGCOverheadLimit >/home/bas/Projects/tradestudent-learn/collect-app/application.out 2>&1 &
