1da3dd2d690ed104d200404d5c854fe6131511bb
Bas Kuis <b@ukora.com>
2019-03-26 06:45:11 -0500

    .

--- learn-app/src/main/groovy/com/ukora/tradestudent/services/learner/TraverseLessonsService.groovy
+++ learn-app/src/main/groovy/com/ukora/tradestudent/services/learner/TraverseLessonsService.groovy
@@ -35,8 +35,8 @@ class TraverseLessonsService {
     public final static long INTERVAL_HOURS = 1
 
     public final static long REPEAT_FOR_TREND = 8 /** Represents hours */
-    public final static long MINIMUM_HOLD_PERIOD = 5 /** In minutes */
-    public final static long REPEAT_FOR_BUY_SELL = 90 /** Represents minutes */
+    public final static long MINIMUM_HOLD_PERIOD = 90 /** In minutes */
+    public final static long REPEAT_FOR_BUY_SELL = 900 /** Represents minutes */
 
     private final static int SIMULATION_INTERVAL_INCREMENT = 1
 
