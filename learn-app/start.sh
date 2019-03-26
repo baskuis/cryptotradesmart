31c7d9171563f0acee4d344443720cc7295c534b
bukora <b@ukora.com>
2019-03-25 19:27:22 -0500

    .

--- learn-app/start.sh
+++ learn-app/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/nohup /usr/bin/java -jar /home/bas/Projects/tradestudent-learn/learn-app/build/libs/learn-app-0.0.1-SNAPSHOT.jar -Xms12000m -Xmx24000m -XX:-UseGCOverheadLimit >/home/bas/Projects/tradestudent-learn/learn-app/application.out 2>&1 &
+/usr/bin/nohup /usr/bin/java -jar /home/bas/Projects/tradestudent-learn/learn-app/build/libs/learn-app.jar -Xms12000m -Xmx24000m -XX:-UseGCOverheadLimit >/home/bas/Projects/tradestudent-learn/learn-app/application.out 2>&1 &
