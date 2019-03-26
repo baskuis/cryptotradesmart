f82fa6e2200a38acb933da90206c4d6b76e00b0c
Bas Kuis <b@ukora.com>
2019-03-25 19:20:59 -0500

    .

--- base-domain/src/main/groovy/com/ukora/domain/entities/AbstractAssociation.groovy
+++ base-domain/src/main/groovy/com/ukora/domain/entities/AbstractAssociation.groovy
@@ -1,9 +1,14 @@
 package com.ukora.domain.entities
 
-import com.mongodb.DBObject
+import org.springframework.data.annotation.Transient
 
 abstract class AbstractAssociation {
 
+    Date date
+
+    Double price
+
+    @Transient
     List<String> intervals = [
             '2minute',
             '5minute',
@@ -18,19 +23,22 @@ abstract class AbstractAssociation {
             '16hour'
     ]
 
+    @Transient
     Exchange exchange
-    String id
-    Date date
-    Double price
-    DBObject dbObject
 
+    @Transient
     Memory memory
+    @Transient
     Twitter twitter
+    @Transient
     List<News> news
+    @Transient
     Map<String, Double> previousPrices = [:]
+    @Transient
     Map<String, Memory> previousMemory = [:]
+    @Transient
     Map<String, List<News>> previousNews = [:]
-
+    @Transient
     Map<String, Map<String, Double>> associations = [:]
 
 }
