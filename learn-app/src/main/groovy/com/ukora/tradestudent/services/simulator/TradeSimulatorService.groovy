415c2b30be8b214f6c619925b86e6016be68b230
Bas Kuis <b@ukora.com>
2019-03-25 21:13:23 -0500

    .

--- learn-app/src/main/groovy/com/ukora/tradestudent/services/simulator/TradeSimulatorService.groovy
+++ learn-app/src/main/groovy/com/ukora/tradestudent/services/simulator/TradeSimulatorService.groovy
@@ -14,9 +14,9 @@ class TradeSimulatorService {
     @Autowired
     BuySellTradingHistoricalSimulatorService buySellTradingHistoricalSimulatorService
 
-    @Scheduled(cron = "0 */6 * * * *")
+    @Scheduled(cron = "0 0 */6 * * *")
     void runSimulation(){
-        Instant current = Instant.now().minus(10, ChronoUnit.DAYS)
+        Instant current = Instant.now().minus(6, ChronoUnit.DAYS)
         Logger.log(String.format("Running simulation starting from %s", Date.from(current)))
         buySellTradingHistoricalSimulatorService.runSimulation(Date.from(current))
     }
