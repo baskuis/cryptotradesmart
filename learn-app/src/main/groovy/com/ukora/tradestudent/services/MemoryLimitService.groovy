2b62d07394a5ba9c67b47071bf0b8993fa2925e1
Bas Kuis <b@ukora.com>
2019-04-19 23:41:04 -0500

    .

--- learn-app/src/main/groovy/com/ukora/tradestudent/services/MemoryLimitService.groovy
+++ learn-app/src/main/groovy/com/ukora/tradestudent/services/MemoryLimitService.groovy
@@ -1,6 +1,7 @@
 package com.ukora.tradestudent.services
 
 import com.ukora.domain.beans.tags.buysell.BuySellTagGroup
+import com.ukora.domain.beans.tags.moves.UpDownMovesTagGroup
 import com.ukora.domain.beans.tags.reversal.UpDownReversalTagGroup
 import com.ukora.domain.beans.tags.trend.UpDownTagGroup
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +14,7 @@ class MemoryLimitService {
     final static MAX_BUY_SELL_COUNT = 200000
     final static MAX_UP_DOWN_COUNT = 2000000
     final static MAX_REVERSAL_COUNT = 100000
+    final static MAX_MOVE_COUNT = 100000
 
     @Autowired
     BytesFetcherService bytesFetcherService
@@ -26,11 +28,15 @@ class MemoryLimitService {
     @Autowired
     UpDownReversalTagGroup upDownReversalTagGroup
 
+    @Autowired
+    UpDownMovesTagGroup upDownMovesTagGroup
+
     @Scheduled(cron = "0 0 * * * *")
     def resetCounts(){
         bytesFetcherService.resetBrainNodesCount(buySellTagGroup, MAX_BUY_SELL_COUNT)
         bytesFetcherService.resetBrainNodesCount(upDownTagGroup, MAX_UP_DOWN_COUNT)
         bytesFetcherService.resetBrainNodesCount(upDownReversalTagGroup, MAX_REVERSAL_COUNT)
+        bytesFetcherService.resetBrainNodesCount(upDownMovesTagGroup, MAX_MOVE_COUNT)
     }
 
 }
