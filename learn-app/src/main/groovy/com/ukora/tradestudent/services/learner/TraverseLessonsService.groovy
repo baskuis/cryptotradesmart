2b3224e9993d876fb6216a5a6e0aa0403608f3c3
Bas Kuis <b@ukora.com>
2019-04-20 21:32:28 -0500

    .

--- learn-app/src/main/groovy/com/ukora/tradestudent/services/learner/TraverseLessonsService.groovy
+++ learn-app/src/main/groovy/com/ukora/tradestudent/services/learner/TraverseLessonsService.groovy
@@ -218,7 +218,7 @@ class TraverseLessonsService {
 
             /** Capture short term up move */
             if (maxMultiple > 0) {
-                Logger.debug("Storing up move lesson date: ${entry['date']} price: ${entry['price']} multiple: ${maxMultiple}")
+                Logger.log("Storing up move lesson date: ${entry['date']} price: ${entry['price']} multiple: ${maxMultiple}")
                 (1..maxMultiple).each {
                     bytesFetcherService.saveLesson(new Lesson(
                             tag: upDownMovesTagGroup?.upMoveTag?.tagName,
@@ -230,7 +230,7 @@ class TraverseLessonsService {
 
             /** Capture short term down move */
             if (minMultiple > 0) {
-                Logger.debug("Storing down move lesson date: ${entry['date']} price: ${entry['price']} multiple: ${minMultiple}")
+                Logger.log("Storing down move lesson date: ${entry['date']} price: ${entry['price']} multiple: ${minMultiple}")
                 (1..minMultiple).each {
                     bytesFetcherService.saveLesson(new Lesson(
                             tag: upDownMovesTagGroup?.downMoveTag?.tagName,
