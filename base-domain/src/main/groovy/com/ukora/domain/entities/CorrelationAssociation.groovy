805b1c2a0ed78ff1429db4353ac6d2e341cc5cd1
Bas Kuis <b@ukora.com>
2019-04-23 23:11:35 -0500

    .

--- base-domain/src/main/groovy/com/ukora/domain/entities/CorrelationAssociation.groovy
+++ base-domain/src/main/groovy/com/ukora/domain/entities/CorrelationAssociation.groovy
@@ -1,13 +1,21 @@
 package com.ukora.domain.entities
 
 import com.ukora.domain.beans.bayes.numbers.NumberAssociationProbability
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Transient
 
 import java.util.concurrent.ConcurrentHashMap
 
 class CorrelationAssociation extends AbstractAssociation {
 
+    @Id
+    String id
+
     Map<String, Double> numericAssociations = new ConcurrentHashMap<>()
+
+    @Transient
     Map<String, Map<String, NumberAssociationProbability>> numericAssociationProbabilities = new ConcurrentHashMap<>()
+
     Map<String, Map<String, Double>> tagScores = new ConcurrentHashMap<>()
     Map<String, Map<String, Double>> tagProbabilities = new ConcurrentHashMap<>()
 
