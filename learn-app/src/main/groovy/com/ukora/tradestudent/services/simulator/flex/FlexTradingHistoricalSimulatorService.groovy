95f56ce702591c09f61db95ba0a48ee318351994
Bas Kuis <b@ukora.com>
2019-04-28 18:38:58 -0500

    .

--- learn-app/src/main/groovy/com/ukora/tradestudent/services/simulator/flex/FlexTradingHistoricalSimulatorService.groovy
+++ learn-app/src/main/groovy/com/ukora/tradestudent/services/simulator/flex/FlexTradingHistoricalSimulatorService.groovy
@@ -45,7 +45,7 @@ class FlexTradingHistoricalSimulatorService extends AbstractTradingHistoricalSim
     @Autowired
     ProbabilityCombinerService probabilityCombinerService
 
-    private final static Double MAX_TRADE_INCREMENT = 0.8
+    private final static Double MAX_TRADE_INCREMENT = 0.6
     private final static Double TRADE_INCREMENT = 0.2
     private final static Double TRADE_TRANSACTION_COST = 0.0020
     private final static Double LOWEST_THRESHOLD = 0.49
@@ -53,7 +53,7 @@ class FlexTradingHistoricalSimulatorService extends AbstractTradingHistoricalSim
     private final static Double THRESHOLD_INCREMENT = 0.01
     private final static Double MAX_THRESHOLD_DELTA = 0.03
     private final static Double MIN_TAG_GROUP_WEIGHT = -0.3
-    private final static Double MAX_TAG_GROUP_WEIGHT = 0.9
+    private final static Double MAX_TAG_GROUP_WEIGHT = 0.6
     private final static Double TAG_GROUP_WEIGHT_INC = 0.3
 
     static class SimulationSettings {
